ecb7097c93615219086c1bdc8b2838a57f098e53
warunafernando <waruna.fernando@gmail.com>
2023-10-07 16:37:29 -0700

    Ready For demo

--- vision_rpl_bot/vision_rpl_bot/cmd_to_romi.py
+++ vision_rpl_bot/vision_rpl_bot/cmd_to_romi.py
@@ -1,8 +1,6 @@
 import rclpy
 from rclpy.node import Node
 from std_msgs.msg import Int16
-import RPi.GPIO as GPIO
-from geometry_msgs.msg import Twist
 from sensor_msgs.msg import Joy
 from vision_rpl_bot.a_star import AStar
 a_star = AStar()
@@ -14,16 +12,19 @@ class RomiSubscriber(Node):
     def __init__(self):
         super().__init__('cmd_romi_subscriber')
         self.subscription = self.create_subscription(Joy, 'joy', self.cmd_to_romi_callback, 10)
+        
  
         
     def cmd_to_romi_callback(self, msg):
-        #print(msg.buttons)
-        #print(msg.axes)
         mx = int(msg.axes[1]*400)
         mz = int(msg.axes[0]*400)
+        redButton = (msg.buttons[0])
+        orangeButton = (msg.buttons[3])
+        blueButton = (msg.buttons[2])
+        greenButton = (msg.buttons[1])
+        a_star.leds(redButton, greenButton, orangeButton)
         print(mx,mz)
         a_star.motors(mx,mz)
-        a_star.leds(msg.buttons[1],msg.buttons[3],msg.buttons[0])
         
 
 
