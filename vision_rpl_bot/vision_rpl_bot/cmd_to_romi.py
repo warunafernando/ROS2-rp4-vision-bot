0873dc946d27750c3394bfb97a2a86e555428bb0
warunafernando <waruna.fernando@gmail.com>
2023-10-07 16:06:14 -0700

    Complete led

--- vision_rpl_bot/vision_rpl_bot/cmd_to_romi.py
+++ vision_rpl_bot/vision_rpl_bot/cmd_to_romi.py
@@ -3,6 +3,7 @@ from rclpy.node import Node
 from std_msgs.msg import Int16
 import RPi.GPIO as GPIO
 from geometry_msgs.msg import Twist
+from sensor_msgs.msg import Joy
 from vision_rpl_bot.a_star import AStar
 a_star = AStar()
 import time
@@ -14,7 +15,7 @@ class RomiSubscriber(Node):
         super().__init__('cmd_romi_subscriber')
         self.subscription = self.create_subscription(Twist,'cmd_vel',self.cmd_to_pwm_callback,10)
         #self.subscription = self.create_subscription(Int16, '/romi', self.listener_callback, 10)
-        #self.subscription = self.create_subscription(, 'joy', self.cmd_to_color_callback, 10)
+        self.subscription = self.create_subscription(Joy, 'joy', self.cmd_to_color_callback, 10)
  
         
  
@@ -22,12 +23,19 @@ class RomiSubscriber(Node):
     def cmd_to_pwm_callback(self, msg):
         mx = int(msg.linear.x*100*400/70)
         mz = int(msg.angular.z*100*400/40)
-        print(mx,mz)
+        #print(mx,mz)
       
-        a_star.motors(mx,mz)
+        #a_star.motors(mx,mz)
         
     def cmd_to_color_callback(self, msg):
-        print(msg)
+        #print(msg.buttons)
+        #print(msg.axes)
+        mx = int(msg.axes[1]*400)
+        mz = int(msg.axes[0]*400)
+        print(mx,mz)
+        a_star.motors(mx,mz)
+        a_star.leds(msg.buttons[1],msg.buttons[3],msg.buttons[0])
+        
         
     #def listener_callback(self, msg):
         #self.get_logger().info('I heard: "%s"' % msg.data)
