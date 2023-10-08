52eae703f04403acbd68006bd4bab457ec719bdf
Hiru <waruna.fernando@gmail.com>
2023-10-07 18:22:23 -0700

    updated

--- vision_rpl_bot/vision_rpl_bot/computer_subscriber.py
+++ vision_rpl_bot/vision_rpl_bot/computer_subscriber.py
@@ -1,7 +1,9 @@
 import rclpy
 from rclpy.node import Node
 
-from std_msgs.msg import Int16
+#from std_msgs.msg import Int16
+from std_msgs.msg import Int16MultiArray
+from array import array
 
 
 class ComputerSubscriber(Node):
@@ -9,14 +11,23 @@ class ComputerSubscriber(Node):
     def __init__(self):
         super().__init__('computer_subscriber')
         self.subscription = self.create_subscription(
-            Int16,
+            Int16MultiArray,
             '/romi',
             self.listener_callback,
             10)
         self.subscription  # prevent unused variable warning
+        
 
     def listener_callback(self, msg):
-        self.get_logger().info('I heard: "%s"' % msg.data)
+        try:
+            data1 = msg.data[0]
+            data2 = msg.data[1]
+            data3 = msg.data[2]
+            print(data1, data2, data3)
+        except:
+            print("Empty array")
+        #self.get_logger().info('I heard: "%s"' % msg.data)
+        
 
 
 def main(args=None):
